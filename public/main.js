c08cdf4556dfb73d227d026de895b8dec9fdd90d
NamTran <phuongnamtran20899@gmail.com>
2024-01-17 10:52:43 +0700

    no message

--- public/main.js
+++ public/main.js
@@ -1,83 +1,91 @@
 $(document).ready(function(){
     $("#plus-1").click(function(){
-        $("#khu-tuu-lau").show();
-        $("#item-1").hide()
+        $("#accordion-title-1").css({"background": "none"});
+        $("#item-1").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
+        $("#plus-1").hide();
+        $("#minus-1").show();
+        $("#accordion-open-content-1").show();
     });
-});
-
-$(document).ready(function(){
     $("#minus-1").click(function(){
-        $("#khu-tuu-lau").hide();
-        $("#item-1").show();
-    })
-});
+        $("#accordion-title-1").css({"background-color": "rgba(1, 13, 124, 0.80)"});
+        $("#item-1").css({"background": "none"});
+        $("#minus-1").hide();
+        $("#plus-1").show();
+        $("#accordion-open-content-1").hide();
+    });
 
-$(document).ready(function(){
     $("#plus-2").click(function(){
-        $("#khu-thi-luyen-moc-nhan").show();
-        $("#item-2").hide()
+        $("#accordion-title-2").css({"background": "none"});
+        $("#item-2").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
+        $("#plus-2").hide();
+        $("#minus-2").show();
+        $("#accordion-open-content-2").show();
     });
-});
-
-$(document).ready(function(){
     $("#minus-2").click(function(){
-        $("#khu-thi-luyen-moc-nhan").hide();
-        $("#item-2").show();
-    })
-});
+        $("#accordion-title-2").css({"background-color": "rgba(1, 13, 124, 0.80)"});
+        $("#item-2").css({"background": "none"});
+        $("#minus-2").hide();
+        $("#plus-2").show();
+        $("#accordion-open-content-2").hide();
+    });
 
-$(document).ready(function(){
     $("#plus-3").click(function(){
-        $("#khu-oot-va-boc-que").show();
-        $("#item-3").hide()
+        $("#accordion-title-3").css({"background": "none"});
+        $("#item-3").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
+        $("#plus-3").hide();
+        $("#minus-3").show();
+        $("#accordion-open-content-3").show();
     });
-});
-
-$(document).ready(function(){
     $("#minus-3").click(function(){
-        $("#khu-oot-va-boc-que").hide();
-        $("#item-3").show();
-    })
-});
+        $("#accordion-title-3").css({"background-color": "rgba(1, 13, 124, 0.80)"});
+        $("#item-3").css({"background": "none"});
+        $("#minus-3").hide();
+        $("#plus-3").show();
+        $("#accordion-open-content-3").hide();
+    });
 
-$(document).ready(function(){
     $("#plus-4").click(function(){
-        $("#khu-quay-chaos").show();
-        $("#item-4").hide()
+        $("#accordion-title-4").css({"background": "none"});
+        $("#item-4").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
+        $("#plus-4").hide();
+        $("#minus-4").show();
+        $("#accordion-open-content-4").show();
     });
-});
-
-$(document).ready(function(){
     $("#minus-4").click(function(){
-        $("#khu-quay-chaos").hide();
-        $("#item-4").show();
-    })
-});
+        $("#accordion-title-4").css({"background-color": "rgba(1, 13, 124, 0.80)"});
+        $("#item-4").css({"background": "none"});
+        $("#minus-4").hide();
+        $("#plus-4").show();
+        $("#accordion-open-content-4").hide();
+    });
 
-$(document).ready(function(){
     $("#plus-5").click(function(){
-        $("#khu-trải-nghiệm-game-mới").show();
-        $("#item-5").hide()
+        $("#accordion-title-5").css({"background": "none"});
+        $("#item-5").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
+        $("#plus-5").hide();
+        $("#minus-5").show();
+        $("#accordion-open-content-5").show();
     });
-});
-
-$(document).ready(function(){
     $("#minus-5").click(function(){
-        $("#khu-trải-nghiệm-game-mới").hide();
-        $("#item-5").show();
-    })
-});
-
-$(document).ready(function(){
-    $("#plus-6").click(function(){
-        $("#checkin").show();
-        $("#item-6").hide()
+        $("#accordion-title-5").css({"background-color": "rgba(1, 13, 124, 0.80)"});
+        $("#item-5").css({"background": "none"});
+        $("#minus-5").hide();
+        $("#plus-5").show();
+        $("#accordion-open-content-5").hide();
     });
-});
 
-$(document).ready(function(){
+    $("#plus-6").click(function(){
+        $("#accordion-title-6").css({"background": "none"});
+        $("#item-6").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
+        $("#plus-6").hide();
+        $("#minus-6").show();
+        $("#accordion-open-content-6").show();
+    });
     $("#minus-6").click(function(){
-        $("#checkin").hide();
-        $("#item-6").show();
-    })
+        $("#accordion-title-6").css({"background-color": "rgba(1, 13, 124, 0.80)"});
+        $("#item-6").css({"background": "none"});
+        $("#minus-6").hide();
+        $("#plus-6").show();
+        $("#accordion-open-content-6").hide();
+    });
 });
