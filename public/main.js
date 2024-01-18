1ce237eeae43edd3c8970b28842fe7bb8e1f3315
NamTran <phuongnamtran20899@gmail.com>
2024-01-18 10:23:18 +0700

    no message

--- public/main.js
+++ public/main.js
@@ -1,91 +1,65 @@
+let details = document.querySelectorAll('.accordion details')
+
+details.forEach(function (d, index) {
+  d.onclick = () => {
+    details.forEach(function(c, i) {
+      index === i ?'':c.removeAttribute('open')
+    });
+  };
+});
+
 $(document).ready(function(){
     $("#plus-1").click(function(){
-        $("#accordion-title-1").css({"background": "none"});
-        $("#item-1").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
         $("#plus-1").hide();
         $("#minus-1").show();
-        $("#accordion-open-content-1").show();
     });
     $("#minus-1").click(function(){
-        $("#accordion-title-1").css({"background-color": "rgba(1, 13, 124, 0.80)"});
-        $("#item-1").css({"background": "none"});
         $("#minus-1").hide();
         $("#plus-1").show();
-        $("#accordion-open-content-1").hide();
     });
 
     $("#plus-2").click(function(){
-        $("#accordion-title-2").css({"background": "none"});
-        $("#item-2").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
         $("#plus-2").hide();
         $("#minus-2").show();
-        $("#accordion-open-content-2").show();
     });
     $("#minus-2").click(function(){
-        $("#accordion-title-2").css({"background-color": "rgba(1, 13, 124, 0.80)"});
-        $("#item-2").css({"background": "none"});
         $("#minus-2").hide();
         $("#plus-2").show();
-        $("#accordion-open-content-2").hide();
     });
 
     $("#plus-3").click(function(){
-        $("#accordion-title-3").css({"background": "none"});
-        $("#item-3").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
         $("#plus-3").hide();
         $("#minus-3").show();
-        $("#accordion-open-content-3").show();
     });
     $("#minus-3").click(function(){
-        $("#accordion-title-3").css({"background-color": "rgba(1, 13, 124, 0.80)"});
-        $("#item-3").css({"background": "none"});
         $("#minus-3").hide();
         $("#plus-3").show();
-        $("#accordion-open-content-3").hide();
     });
 
     $("#plus-4").click(function(){
-        $("#accordion-title-4").css({"background": "none"});
-        $("#item-4").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
         $("#plus-4").hide();
         $("#minus-4").show();
-        $("#accordion-open-content-4").show();
     });
     $("#minus-4").click(function(){
-        $("#accordion-title-4").css({"background-color": "rgba(1, 13, 124, 0.80)"});
-        $("#item-4").css({"background": "none"});
         $("#minus-4").hide();
         $("#plus-4").show();
-        $("#accordion-open-content-4").hide();
     });
 
     $("#plus-5").click(function(){
-        $("#accordion-title-5").css({"background": "none"});
-        $("#item-5").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
         $("#plus-5").hide();
         $("#minus-5").show();
-        $("#accordion-open-content-5").show();
     });
     $("#minus-5").click(function(){
-        $("#accordion-title-5").css({"background-color": "rgba(1, 13, 124, 0.80)"});
-        $("#item-5").css({"background": "none"});
         $("#minus-5").hide();
         $("#plus-5").show();
-        $("#accordion-open-content-5").hide();
     });
 
     $("#plus-6").click(function(){
-        $("#accordion-title-6").css({"background": "none"});
-        $("#item-6").css({"background-color": "rgba(1, 13, 124, 0.80)", "width": "80%", "height": "100%", "border-radius": "10px"});
         $("#plus-6").hide();
         $("#minus-6").show();
-        $("#accordion-open-content-6").show();
     });
     $("#minus-6").click(function(){
-        $("#accordion-title-6").css({"background-color": "rgba(1, 13, 124, 0.80)"});
-        $("#item-6").css({"background": "none"});
         $("#minus-6").hide();
         $("#plus-6").show();
-        $("#accordion-open-content-6").hide();
     });
 });
